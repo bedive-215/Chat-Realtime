a7abd0f8a83d690d4e39b202545c6da9fd77eda3
bedive <trungbed1ve@gmail.com>
2025-08-26 21:20:41 +0700

    feat(configs): add configs for env, database and cloudinary

---
+++ Backend/configs/envConf.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+export const {
+    CLOUD_NAME,
+    API_KEY,
+    API_SECRET_KEY,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST
+} = process.env;

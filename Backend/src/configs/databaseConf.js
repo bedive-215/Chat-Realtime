90639fcfe04349d072d0ff6bb8ed2cf1e7c499b7
bedive <trungbed1ve@gmail.com>
2025-11-05 11:09:34 +0700

    fix: docker-compose and dependence

--- Backend/src/configs/databaseConf.js
+++ Backend/src/configs/databaseConf.js
@@ -1,15 +1,17 @@
 import { Sequelize } from "sequelize";
-import { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } from '../helpers/env.helper.js';
-
-const sequelize = new Sequelize(
+import {
   DB_NAME,
-  DB_USER ,
-  DB_PASSWORD || '',
-  {
-    host: DB_HOST || 'localhost',
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+} from "../helpers/env.helper.js";
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST || "localhost",
+  port: DB_PORT || 3306,
+  dialect: "mysql",
+  logging: false,
+});
 
 export default sequelize;

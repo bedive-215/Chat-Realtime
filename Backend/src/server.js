71c9f44572679cd6aff7b0a21d647153f4775349
bedive <trungbed1ve@gmail.com>
2026-04-20 23:18:29 +0700

    fix: change url axios and allow cors url

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -12,6 +12,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://frontend:5173",
+  "https://chat-realtime.pages.dev",
 ];
 
 app.use(
--- Frontend/src/lib/axio.js
+++ Frontend/src/lib/axio.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api` , 
+    baseURL: 'https://api-chat-realtime-gp3w.onrender.com/api', 
     withCredentials: true,
 });
